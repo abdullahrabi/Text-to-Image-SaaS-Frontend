0dc9bdb028daa1ad3e582608bdebb3704fb0c2cf
Abdullah Rabi <abdullahrabi026@gmail.com>
2025-08-18 01:48:30 +0500

    Add Footer logo to redirect to HomePage

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import {assets} from '../assets/assets'
+import { Link } from 'react-router-dom'
 const Footer = () => {
   return (
     <div className='flex items-center justify-between gap-4  py-3 mt-20'>
+      <Link to='/'>
         <img  width={150}  src={assets.logo} alt="" />
+        </Link>
         <p className='flex-1 border-l border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden'>Copyright @Abdullah Rabi.dev | All right reserved.</p>
         <div className='flex gap-2.5 '>
             <img width={35} src={assets.facebook_icon} alt="" />
