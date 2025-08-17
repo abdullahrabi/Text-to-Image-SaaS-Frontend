087240c7717daec0eb5026ff4b68d2078188b6af
Abdullah Rabi <abdullahrabi026@gmail.com>
2025-08-18 02:10:23 +0500

    Remove Captcha background

--- src/components/CaptchaModal.jsx
+++ src/components/CaptchaModal.jsx
@@ -11,9 +11,9 @@ const CaptchaModal = ({ onVerify, onClose }) => {
   }, []);
 
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center backdrop-blur-md bg-black/40">
-      <div className="bg-white p-6 rounded-2xl shadow-lg w-[90%] max-w-md text-center">
-        <h2 className="text-lg font-semibold mb-4">Verify Captcha</h2>
+    <div className="">
+      
+       
         
         <Turnstile
           sitekey={import.meta.env.VITE_TURNSTILE_SITE_KEY}
@@ -24,13 +24,8 @@ const CaptchaModal = ({ onVerify, onClose }) => {
           theme="dark"
         />
 
-        <button
-          onClick={onClose}
-          className="mt-4 px-4 py-2 bg-gray-200 rounded-lg hover:bg-gray-300"
-        >
-          Cancel
-        </button>
-      </div>
+       
+      
     </div>
   );
 };
